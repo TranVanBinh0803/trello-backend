e39317963304cae4c66099308b3c8907c0153899
TranVanBinh0803 <tranvanbinh08082003@gmail.com>
2026-06-29 19:21:27 +0700

    chore: update something

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,14 @@
-export const WHITELIST_DOMAINS = ["http://localhost:5173"];
+const parseWhitelistDomains = () => {
+  const envDomains = process.env.WHITELIST_DOMAINS || process.env.APP_CLIENT_URL;
+  if (!envDomains) return ["http://localhost:5173"];
+
+  return envDomains
+    .split(",")
+    .map((domain) => domain.trim())
+    .filter(Boolean);
+};
+
+export const WHITELIST_DOMAINS = parseWhitelistDomains();
 export const BOARD_TYPES = {
   PUBLIC: "public",
   PRIVATE: "private",
