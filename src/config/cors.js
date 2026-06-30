cf7b22841e41cf246965e03f6df59ff56c9a87d8
TranVanBinh0803 <tranvanbinh08082003@gmail.com>
2026-06-30 16:53:23 +0700

    fix: cors for VNPAY redirect

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,19 +1,26 @@
-import { WHITELIST_DOMAINS } from '~/utils/constants'
-import { StatusCodes } from 'http-status-codes'
-import { ApiError } from '~/utils/types'
+import { WHITELIST_DOMAINS } from "~/utils/constants";
+import { StatusCodes } from "http-status-codes";
+import { ApiError } from "~/utils/types";
 
 export const corsOptions = {
   origin: function (origin, callback) {
-    // Cho phép gọi API bằng POSTMAN trên môi trường dev
-    if (!origin && process.env.BUILD_MODE === 'dev') {
-      return callback(null, true)
+    // Requests without Origin are not browser CORS requests.
+    // Examples: VNPAY return redirect, Postman, curl, health checks.
+    if (!origin) {
+      return callback(null, true);
     }
+
     if (WHITELIST_DOMAINS.includes(origin)) {
-      return callback(null, true)
+      return callback(null, true);
     }
-    return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`))
+
+    return callback(
+      new ApiError(
+        StatusCodes.FORBIDDEN,
+        `${origin} not allowed by our CORS Policy.`
+      )
+    );
   },
   optionsSuccessStatus: 200,
-  //   CORS sẽ cho phép nhận cookies từ request
-  credentials: true
-}
+  credentials: true,
+};
